9a6c0605f736e1cdbd4a123d8ba00acf80907f71
mike73192 <214967078+mike73192@users.noreply.github.com>
2025-10-30 19:08:10 +0900

    Configure Vite path alias for src

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
+import { fileURLToPath, URL } from 'node:url';
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 5173
