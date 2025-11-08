f4fa678756eacf37815434567fc81ca36c49eff8
mike73192 <214967078+mike73192@users.noreply.github.com>
2025-11-08 22:34:36 +0900

    Fix asset paths for static preview

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
